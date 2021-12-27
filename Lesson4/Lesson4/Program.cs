a43c0ec19dfcde36c9f24ee4cf806c3c883f8d88
tms@armada3d.ru <tms@armada3d.ru>
2021-12-27 23:34:11 +0300

    Реализовал ввод данных в созданный массив

--- Lesson4/Lesson4/Program.cs
+++ Lesson4/Lesson4/Program.cs
@@ -8,14 +8,55 @@ namespace Lesson4
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            CreateArray();
+           var array=CreateArray();
+           InputData(ref array);
         }
 
-        public static void CreateArray()
-        { 
+        public static decimal[,] CreateArray()
+        {
+            Console.WriteLine("Введите количество строк в массиве");
+            var rows = Console.ReadLine();
+            Console.WriteLine("Введите количество столбцов в массиве");
+            var columns = Console.ReadLine();
 
+            if (!int.TryParse(rows, out int rowsResult) || !int.TryParse(columns, out int columnsResult) || rowsResult<=1 || columnsResult<=1)
+            {
+                Console.WriteLine("Ошибка, необходимо ввести число >=1");
+                Console.ReadKey();
+                return null;
+            }
+
+            Console.WriteLine($"Создаем массив {rowsResult}х{columnsResult}");
+            decimal[,] tmsArray = new decimal[rowsResult, columnsResult];
+
+            return tmsArray;
         }
-    }
+        public static void InputData(ref decimal[,] array)
+        {
+             int rows = array.GetUpperBound(0) + 1;
+
+            for (int j = 0; j < array.Length / rows;)
+            {
+                for (int i = 0; i < rows;)
+                {
+                    Console.WriteLine($"Для строки {j + 1}, столбца {i + 1} введите число");
+
+                    if (decimal.TryParse(Console.ReadLine(), out decimal inputRowsValue))
+                    {
+                        array[j, i] = inputRowsValue;
+                        i++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка, вввода данных.Попробуем еще раз.");
+                    }
+                }
+                j++;
+            }
+
+            Console.ReadKey();
+        }}
 }
