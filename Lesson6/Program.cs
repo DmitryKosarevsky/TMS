0374a5162c30094c595c43ca151de96fbbab123f
tms@armada3d.ru <tms@armada3d.ru>
2022-01-13 00:17:49 +0300

    Реализовано. Рефакторинг кода матрицы

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,110 +1,199 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 
-namespace Lesson5
+namespace Lesson6
 {
-    class Program
+    internal class Program
     {
-        public static string InputString()
-        {
-            string userInput;
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Введите строку содержащую буквы латинского алфавита, знаки препинания и цифры : ");
-                userInput = Console.ReadLine();
-            } while (string.IsNullOrEmpty(userInput));
-            return userInput;
-        }
-
         static void Main(string[] args)
         {
-            var userInput = InputString();
-            const string infoText =
-                "Спасибо за строку.\nВ строке мы должны :\n" +
-                "1) Найти слова, содержащие максимальное количество цифр.\n" +
-                "2) Найти самое длинное слово и определить, сколько раз оно встретилось в тексте.\n" +
-                "3) Заменить цифры от 0 до 9 на слова «ноль», «один», …, «девять».\n" +
-                "4) Вывести на экран сначала вопросительные, а затем восклицательные предложения.\n" +
-                "5) Вывести на экран только предложения, не содержащие запятых.\n" +
-                "6) Найти слова, начинающиеся и заканчивающиеся на одну и ту же букву.\n\n"+
-                "Введите цифру : ";
+            var tmsArray = CreateArray();
+            InputData(ref tmsArray);
+            Console.Clear();
+            ViewArray(ref tmsArray);
 
-            Console.WriteLine(infoText);
+            const string textInfo =
+                "Сделайте ваш выбор:\n" +
+                "1) Найти количество положительных/отрицательных чисел в матрице\n" +
+                "2) Сортировка по элементов матрицы построчно\n" +
+                "3) Инверсия элементов матрицы построчно\n" +
+                "4) Выход\n\n" +
+                "Выберите действие: ";
+            Console.Write(textInfo);
 
             switch (Console.ReadLine())
             {
                 case "1":
-                    string[] arrWords = userInput.Split(new char[] { ' ', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
-                    var arrSorted = arrWords.Where(x => double.TryParse(x, out double userInputResult)).OrderByDescending(s => s.Length).ToList();
+                    var tuple = CountPositiveOrNegative(ref tmsArray);
+                    Console.WriteLine($"Положительных чисел {tuple.positive}. Отрицательных {tuple.negative}");
+                    break;
+                case "2":
+                    SortArray(ref tmsArray, true);
+                    Console.WriteLine("Вывожу отсортированный массив по возрастанию");
+                    ViewArray(ref tmsArray);
 
-                    if (arrSorted.Count > 0)
+                    SortArray(ref tmsArray, false);
+                    Console.WriteLine("Вывожу отсортированный массив по убыванию");
+                    ViewArray(ref tmsArray);
+                    break;
+                case "3":
+                    InvertArray(ref tmsArray);
+                    Console.WriteLine("Вывожу массив инвертированный построчно");
+                    ViewArray(ref tmsArray);
+                    break;
+                case "4":
+                    Console.WriteLine("Good Bye!");
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        /// <summary>Метод инветрирования массива</summary>
+        /// <param name="tmsArray">массив по ссылке</param>
+        /// <returns></returns>
+        private static void InvertArray(ref decimal[,] tmsArray)
+        {
+            int row = tmsArray.GetUpperBound(0) + 1;
+            for (int i = 0; i < row; i++)
+            {
+                int l = tmsArray.Length / row; // длина массива
+                int k = l / 2;          // середина массива
+                for (int j = 0; j < k; j++)
+                {
+                    Swap(ref tmsArray[i, j], ref tmsArray[i, l - j - 1]);
+                }
+            }
+        }
+
+        /// <summary>Сортировка построчно массива</summary>
+        /// <param name="tmsArray">массив по ссылке</param>
+        /// <param name="direction">true - по возрастанию</param>
+        /// <returns></returns>
+        private static void SortArray(ref decimal[,] tmsArray, bool direction = true)
+        {
+            int rows = tmsArray.GetUpperBound(0) + 1;
+
+            //Сортировка 
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < tmsArray.Length / rows; j++)
+                {
+                    for (var g = 0; g < tmsArray.Length / rows - j - 1; g++)
                     {
-                        var maxLength = arrSorted[0].Length;
-                        Console.WriteLine("Самые длинные числа: " + string.Join(", ", arrSorted.Where(x => x.Length == maxLength)));
+                        if (direction) //true - по возрастанию
+                        {
+                            if (tmsArray[i, g] > tmsArray[i, g + 1])
+                            {
+                                Swap(ref tmsArray[i, g], ref tmsArray[i, g + 1]);
+                            }
+                        }
+                        else
+                        {
+                            if (tmsArray[i, g] < tmsArray[i, g + 1])
+                            {
+                                Swap(ref tmsArray[i, g], ref tmsArray[i, g + 1]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>Метод обмена элементов </summary>
+        /// <param name="e1">первый элемент</param>
+        /// <param name="e2">второй элемент</param>
+        static void Swap(ref decimal e1, ref decimal e2)
+        {
+            var temp = e1; // вспомогательный элемент для обмена значениями
+            e1 = e2;
+            e2 = temp;
+        }
+
+        public static (int positive, int negative) CountPositiveOrNegative(ref decimal[,] tmsArray)
+        {
+            int positive = 0;
+            int negative = 0;
+            int rows = tmsArray.GetUpperBound(0) + 1;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < tmsArray.Length / rows; j++)
+                {
+                    var result = Math.Sign(tmsArray[i, j]);
+
+                    if (result > 0)
+                    {
+                        positive++;
                     }
                     else
                     {
-                        Console.WriteLine("В строке нет чисел");
-                    }
-                    break;
-                case "2":
-                    string[] arrLongWord = userInput.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                    var longWord = arrLongWord.OrderByDescending(x => x.Length).FirstOrDefault();
-                    Console.WriteLine($"Самое длинное слово : {longWord} встретилось {arrLongWord?.Where(x => x.Contains(longWord)).Count()} раз.");
-                    break;
-                case "3":
-                    Console.WriteLine(userInput
-                        .Replace("1", "один")
-                        .Replace("2", "два")
-                        .Replace("3", "три")
-                        .Replace("4", "четыре")
-                        .Replace("5", "пять")
-                        .Replace("6", "шесть")
-                        .Replace("7", "семь")
-                        .Replace("8", "восемь")
-                        .Replace("9", "девять")
-                        .Replace("0", "ноль"));
-                    break;
-                case "4":
-                    Regex re = new Regex(@"(\w+\s*)+[!?]");
-                    List<string> strings = new List<string>();
-                    foreach (Match item in re.Matches(userInput))
-                    {
-                        strings.Add(item.Value);
-                    }
-                    foreach (string item in strings.OrderBy((x) => !x.EndsWith("?")))
-                    {
-                        Console.WriteLine(item);
-                    }
-                    break;
-                case "5":
-                    string[] arraySentence = userInput.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
-                    foreach (string item in arraySentence)
-                    {
-                        if (!item.Contains(','))
+                        if (result < 0)
                         {
-                            Console.WriteLine(item);
+                            negative++;
                         }
                     }
-                    break;
-                case "6":
-                    string[] arrWord = userInput.Split(new char[] { '.', ',', '!', '?', ' ' });
 
-                    foreach (var item in arrWord)
-                    {
-                        if (item[0] == item[item.Length - 1]) Console.WriteLine(item);
-                    }
+                }
+            }
+            return (positive, negative);
+        }
 
-                    break;
-                default:
-                    Console.WriteLine("Неверный выбор");
-                    break;
+        public static void ViewArray(ref decimal[,] tmsArray)
+        {
+            int rows = tmsArray.GetUpperBound(0) + 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < tmsArray.Length / rows; j++)
+                {
+                    Console.Write($"{tmsArray[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static decimal[,] CreateArray()
+        {
+            Console.WriteLine("Введите количество строк в массиве");
+            var rows = Console.ReadLine();
+            Console.WriteLine("Введите количество столбцов в массиве");
+            var columns = Console.ReadLine();
+
+            if (!int.TryParse(rows, out int rowsResult) || !int.TryParse(columns, out int columnsResult) || rowsResult <= 1 || columnsResult <= 1)
+            {
+                Console.WriteLine("Ошибка, необходимо ввести число >=1");
+                Console.ReadKey();
+                return null;
             }
 
-            Console.ReadLine();
+            return new decimal[rowsResult, columnsResult];
         }
+        public static void InputData(ref decimal[,] tmsArray)
+        {
+            int rows = tmsArray.GetUpperBound(0) + 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < tmsArray.Length / rows;)
+                {
+                    Console.Clear();
+                    ViewArray(ref tmsArray);
+                    Console.WriteLine($"Для строки {i + 1}, столбца {j + 1} введите число");
+
+                    if (decimal.TryParse(Console.ReadLine(), out decimal inputRowsValue))
+                    {
+                        tmsArray[i, j] = inputRowsValue;
+                        j++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка, вввода данных.Попробуем еще раз.");
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+
     }
 }
